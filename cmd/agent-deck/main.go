b0c8286d3ada83a590bbdc4012823d3b067dd03e
chenwl <qzchenwl@gmail.com>
2026-03-18 22:01:46 +0800

    chore: release 0.26.3-cwl.2

--- cmd/agent-deck/main.go
+++ cmd/agent-deck/main.go
@@ -29,7 +29,7 @@ import (
 	"github.com/asheshgoplani/agent-deck/internal/web"
 )
 
-const Version = "0.26.3-cwl.1"
+const Version = "0.26.3-cwl.2"
 
 // Table column widths for list command output
 const (
